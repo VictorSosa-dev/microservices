a1687c9f183e8eb58a5340d89875b9f1b7b48701
Victor Sosa <manuelsosapia@gmail.com>
2022-06-08 22:02:39 -0500

    circuit Breaker

--- usuario-service/src/main/java/com/usuario/service/servicio/UsuarioService.java
+++ usuario-service/src/main/java/com/usuario/service/servicio/UsuarioService.java
@@ -32,12 +32,12 @@ public class UsuarioService {
 
 	// Rest Template
 	public List<Carro> getCarros(int usuarioId) {
-		List<Carro> carros = restTemplate.getForObject("http://carro-service/carro/user/" + usuarioId, List.class);
+		List<Carro> carros = restTemplate.getForObject("http://carro-service/carro/usuario/" + usuarioId, List.class);
 		return carros;
 	}
 
 	public List<Moto> getMotos(int usuarioId) {
-		List<Moto> motos = restTemplate.getForObject("http://moto-service/moto/user/" + usuarioId, List.class);
+		List<Moto> motos = restTemplate.getForObject("http://moto-service/moto/usuario/" + usuarioId, List.class);
 		return motos;
 	}
 
