65fa1d771170c0392840d781d69ff3ba2bdfcd89
Victor Sosa <manuelsosapia@gmail.com>
2022-06-08 21:17:18 -0500

    circuit breaker

--- gateway-service/src/main/java/com/gateway/service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/gateway/service/GatewayServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class GatewayServiceApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
